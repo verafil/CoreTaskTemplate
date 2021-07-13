df6dac93e3e5456f13a8553c2f389ef15ecc3305
verafil <9601604@gmail.com>
2021-07-13 16:02:12 +0300

    PP 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Vera", "Olegovna", (byte) 29);
+        userService.saveUser("Serg", "Sergeevich", (byte) 34);
+        userService.saveUser("Svetlana", "Ivanovna", (byte) 20);
+        userService.saveUser("Igor", "Sergeevich", (byte) 28);
+
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+        userService.getConnection().close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    Connection connection;
+    Driver driver;
+    private static final String URL = "jdbc:mysql://localhost:3306/db_test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Util() {
+        try {
+            driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Подключение открыто!");
+            }
+        } catch (SQLException throwables) {
+            System.err.println("Не удалось загрузить класс драйвера!");
+        }
+
+    }
 }
