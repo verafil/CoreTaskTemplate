d7ae674551bd7240acd7d562f74e3c417ff7a979
verafil <9601604@gmail.com>
2021-07-15 18:31:11 +0300

    PP 1.1.3 fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,19 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Vera", "Olegovna", (byte) 29);
-        userService.saveUser("Serg", "Sergeevich", (byte) 34);
-        userService.saveUser("Svetlana", "Ivanovna", (byte) 20);
-        userService.saveUser("Igor", "Sergeevich", (byte) 28);
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Vera", "Olegovna", (byte) 29);
+        userDaoJDBC.saveUser("Serg", "Sergeevich", (byte) 34);
+        userDaoJDBC.saveUser("Svetlana", "Ivanovna", (byte) 20);
+        userDaoJDBC.saveUser("Igor", "Sergeevich", (byte) 28);
 
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
-        userService.getConnection().close();
+        System.out.println(userDaoJDBC.getAllUsers());
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
+        userDaoJDBC.getConnection().close();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,109 +1,36 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
-import java.util.LinkedList;
 import java.util.List;
 
-public class UserServiceImpl extends Util implements UserDao, UserService {
-    Connection connection = getConnection();
+public class UserServiceImpl extends Util implements UserService {
 
-    @Override
+    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-        String sql = "create table users\n" +
-                "(\n" +
-                "\tid int auto_increment,\n" +
-                "\tname varchar(45) not null,\n" +
-                "\tlastName varchar(45) not null,\n" +
-                "\tage int not null,\n" +
-                "\tconstraint users_pk\n" +
-                "\t\tprimary key (id)\n" + ")";
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            try {
-                preparedStatement.executeUpdate();
-            } catch (SQLSyntaxErrorException s) {
-                System.err.println("Создание не возможно. Таблица с таким наименованием уже существует.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.createUsersTable();
     }
 
-    @Override
     public void dropUsersTable() {
-        String sql = "drop table users";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            try {
-                preparedStatement.executeUpdate();
-            } catch (SQLSyntaxErrorException s) {
-                System.err.println("Удаление не возможно. Таблица с таким наименованием не существует.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.dropUsersTable();
     }
 
-    @Override
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "insert into users (name, lastName, age) values(?,?,?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-
-            System.out.println("User с именем – " + name + " добавлен в базу данных");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
-    @Override
     public void removeUserById(long id) {
-        String sql = "delete from users where id = ?";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.removeUserById(id);
     }
 
-    @Override
     public List<User> getAllUsers() {
-        String sql = "select * from users";
-        List<User> list = new LinkedList<>();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = preparedStatement.executeQuery(sql);
-
-            while (resultSet.next()) {
-                User user = new User();
-                user.setName(resultSet.getString(2));
-                user.setId(resultSet.getLong(1));
-                user.setAge(resultSet.getByte(4));
-                user.setLastName(resultSet.getString(3));
-
-                list.add(user);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return list;
+        return userDaoJDBC.getAllUsers();
     }
 
-    @Override
     public void cleanUsersTable() {
-        String sql = "delete from users";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        userDaoJDBC.cleanUsersTable();
     }
 }
