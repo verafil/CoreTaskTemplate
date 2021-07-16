bae8cd8fa5890d464a45c08b235c8f0527919470
verafil <9601604@gmail.com>
2021-07-16 10:51:42 +0300

    fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
-public class UserServiceImpl extends Util implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
